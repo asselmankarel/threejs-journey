4c1c3dab28e1c7c5e5e6cf3b65603b9b610fb132
asselmankarel <asselman.karel@gmail.com>
2026-04-18 13:42:55 +0200

    refactor: organize and enhance Three.js scene setup with grouped cubes and axes helper

--- 04-transform-objects/src/script.js
+++ 04-transform-objects/src/script.js
@@ -1,39 +1,63 @@
-import * as THREE from 'three'
+import * as THREE from "three";
 
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl");
+const scene = new THREE.Scene();
 
-// Scene
-const scene = new THREE.Scene()
+//objects
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+const axcesHelper = new THREE.AxesHelper(2);
 
-/**
- * Objects
- */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const group = new THREE.Group();
+// mesh.position.x = 0.7;
+// mesh.position.y = -0.6;
+// mesh.position.z = -0.5;
 
-/**
- * Sizes
- */
+scene.add(axcesHelper);
+
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 }),
+);
+cube1.position.x = 2;
+group.add(cube1);
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 }),
+);
+cube2.position.x = 0;
+group.add(cube2);
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff }),
+);
+cube3.position.x = -2;
+group.add(cube3);
+
+scene.add(group);
+group.position.y = 1;
+group.scale.y = 2;
+group.rotation.y = 0.3;
+/* camera and renderding */
 const sizes = {
-    width: 800,
-    height: 600
-}
+  width: 800,
+  height: 600,
+};
 
-/**
- * Camera
- */
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
-scene.add(camera)
+// camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+// camera.position.z = 3;
+// camera.position.x = 1;
+// camera.position.y = 1;
+camera.position.set(0, 0, 4);
+scene.add(camera);
 
-/**
- * Renderer
- */
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+//camera.lookAt(mesh.position);
+
+// render
+const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
