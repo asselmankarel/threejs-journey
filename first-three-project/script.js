4bc0eba21a39f3efc8533820a46e53a29fed1682
asselmankarel <asselman.karel@gmail.com>
2026-04-18 12:05:21 +0200

    implement basic Three.js scene with a red cube and canvas setup

--- first-three-project/script.js
+++ first-three-project/script.js
@@ -1,2 +1,29 @@
 import * as THREE from "three";
 
+const canvas = document.querySelector("canvas.webgl");
+const scene = new THREE.Scene();
+const mesh = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: "red" }),
+);
+
+scene.add(mesh);
+
+const size = {
+  width: 800,
+  height: 600,
+  aspect: () => size.width / size.height,
+};
+
+const camera = new THREE.PerspectiveCamera(75, size.aspect());
+camera.position.z = 3;
+
+scene.add(camera);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+
+renderer.setSize(size.width, size.height);
+
+renderer.render(scene, camera);
